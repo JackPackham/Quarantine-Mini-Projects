165886b9a46bb9be8f8795da1475c86bac75f899
JackPackham <60839411+JackPackham@users.noreply.github.com>
2020-04-28 14:09:34 +0100

    Tesseract implementation
    Using tesseract for OCR of png files.

--- abstractor/concept.py
+++ abstractor/concept.py
@@ -1,19 +1,33 @@
-class file(object):
+class tesseract():
 
-    def __init__(self, FILE):
-        self.file = FILE
-        self.final = ""
+    def imageMake(self,arg,imageLocal):
+        self.image = cv2.imread(imageLocal)
+        self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+        if arg == "threshold":
+            self.gray = cv2.threshold(self.gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+        elif arg == "blur":
+            self.gray = cv2.medianBlur(self.gray, 3)
+
+        else:
+            print("incorrect parse")
+            cv2.waitKey(0)
+
+        self.filename = "{}.png".format(os.getpid())
+        cv2.imwrite(self.filename, self.gray)
 
 
 
-    def readFile(self):
-        try:
-            self.fileText = docx.Document(self.file)
-            self.read = True
-        except:
-            print(f"couldnt read {self.file}")
-    
+    def imgToString(self):
+        self.final=""
+        self.text = pytesseract.image_to_string(Image.open(self.filename))
+        os.remove(self.filename)
+        print(self.text)
 
+        cv2.imshow("Image", self.image)
+        cv2.imshow("Output", self.gray)
+        self.read = True
 
     def createFile(self):
         with open("abstracted.txt", "x") as f:
@@ -23,7 +37,7 @@ class file(object):
 
     def abstract(self):
         if self.read == True:
-            for word in self.fileText.split():
+            for word in self.text.split():
                 print(word)
                 if self.switch(word) == False:
                     self.final += " "+word
@@ -38,27 +52,28 @@ class file(object):
 
         switches = ["and","as","a","for","if","but","of",
                     "the","that","which","is","was", "i",
-                    "in", "with", "an", "or"]
+                    "in", "with", "an", "or", "to"]
         if arguement in switches:
             return True
         else:
             return False
 
-
-
+    
 
 if __name__ == "__main__":
 
-    try: 
-        import docx
+    try:
+        from PIL import Image
+        import pytesseract
+        import imutils
+        import os
+        import cv2
     except:
-        print("python docx not installed")
+        print("imports failed")
 
-    #created new file class and initialised
-    try1 = file("dummy.docx")
-    #read the file it was given
-    try1.readFile()
-    #abstracting file
-    try1.abstract()
-    #writing abstracted file
-    try1.createFile()
+    tess = tesseract()
+    tess.imageMake("threshold", "dummy.png")
+    tess.imgToString()
+    tess.abstract()
+    tess.createFile()
+
